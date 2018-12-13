4d3431881cd3e6f944257122e869a8b03c7d18fb
Touhid Mia <touhid@bitmascot.com>
2018-12-13 18:50:03 +0600

    bad code

--- react-app/src/views/dashboard-view.js
+++ react-app/src/views/dashboard-view.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 import RaViewComponent from "../artifacts/ra-view-component";
+import { Chart } from "react-charts";
 
 
 const styles = theme => ({
@@ -12,7 +13,31 @@ const styles = theme => ({
 class DashboardView extends RaViewComponent {
     appRender () {
         const { classes } = this.props;
-        return <h1 className={classes.root}>Dashboard View</h1>;
+        return (
+            <div
+                style={{
+                    width: "800px",
+                    height: "500px"
+                }}
+            >
+                <Chart
+                    data={[
+                        {
+                            label: "Series 1",
+                            data: [[0, 1], [1, 2], [2, 10], [3, 2], [4, 7]]
+                        },
+                        {
+                            label: "Series 2",
+                            data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+                        }
+                    ]}
+                    axes={[
+                        { primary: true, type: "linear", position: "bottom" },
+                        { type: "linear", position: "left" }
+                    ]}
+                />
+            </div>
+        );
     }
 }
 export default withStyles(styles)(DashboardView);
