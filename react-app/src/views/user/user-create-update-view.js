795dc3e633780738bf5c60144ca65f2efaaf5c4e
Touhid Mia <hmtm.cse@gmail.com>
2018-12-16 17:06:36 +0600

    Resolve URL Mapping

--- react-app/src/views/user/user-create-update-view.js
+++ react-app/src/views/user/user-create-update-view.js
@@ -1,18 +1,10 @@
 import RaViewComponent from "../../artifacts/ra-view-component";
 import React from "react";
 import {
-    Button,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableRow,
-    TextField,
-    Typography,
-    withStyles
-} from "@material-ui/core";
-import RaTableHeader from "../../artifacts/ra-table-header";
-import RaPagination from "../../artifacts/ra-pagination";
+    Button, TextField, FormControl, InputLabel, Radio,
+    Select,MenuItem, FormControlLabel, Checkbox, FormGroup, FormLabel,RadioGroup,
+    Card, CardContent, CardActions, CardHeader, Grid, withStyles
+} from '@material-ui/core'
 
 
 const styles = theme => ({
@@ -42,22 +34,112 @@ const styles = theme => ({
 
 class UserCreateUpdateView extends RaViewComponent {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            countrySelect: 'bangladesh',
+            states: [],
+            genderRadio: '',
+        };
+    }
+
+
+    countrySelectChange = event => {
+        this.setState({countrySelect:event.target.value});
+    };
+
+    statesSelectChange = event => {
+        this.setState({states:event.target.value});
+    };
+
+    genderRadioFill = event => {
+        this.setState({genderRadio:event.target.value});
+    };
+
+
     appRender() {
-        const {classes} = this.props;
-        return (<React.Fragment>
-            <Paper className={classes.mainActionArea}>
-                <div>
-                    <Typography variant="headline">Users</Typography>
-                </div>
-                <div>
-                    <form className={classes.displayInline}>
-                        <TextField placeholder="search" name="search"/>
-                    </form>
-                    <Button className={classes.marginToLeft} onClick={event =>{this.goToUrl("/user", event)}} variant="contained" color="primary">Cancel</Button>
-                    <Button className={classes.marginToLeft} variant="contained" color="primary">Reload</Button>
-                </div>
-            </Paper>
-        </React.Fragment>);
+        const { classes } = this.props;
+        const registrationForm = (
+            <form>
+                <Card>
+                    <CardHeader title="Create User"/>
+                    <CardContent>
+
+                        <Grid container spacing={8}>
+                            <Grid item xs={12}><TextField label="First name" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="Last name" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="Mobile" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="website" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="Email" type="email" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="Birthday" fullWidth/></Grid>
+                            <Grid item xs={6}><TextField label="Password" type="password" fullWidth/></Grid>
+                            <Grid item xs={6}>
+                                <FormControl fullWidth>
+                                    <InputLabel>Country</InputLabel>
+                                    <Select value={this.state.countrySelect} onChange={this.countrySelectChange}>
+                                        <MenuItem value="bangladesh">Bangladesh</MenuItem>
+                                        <MenuItem value="australia">Australia</MenuItem>
+                                        <MenuItem value="usa">USA</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FormControl fullWidth>
+                                    <InputLabel>States</InputLabel>
+                                    <Select value={this.state.states} onChange={this.statesSelectChange} multiple>
+                                        <MenuItem value="bangladesh">Bangladesh</MenuItem>
+                                        <MenuItem value="australia">Australia</MenuItem>
+                                        <MenuItem value="usa">USA</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <FormGroup row>
+                                    <FormControlLabel control={<Checkbox value="checkedC" />} label="Single Checkbox" />
+                                </FormGroup>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormGroup row>
+                                    <FormControlLabel control={<Checkbox value="checkbox1" />} label="Checkbox 1"/>
+                                    <FormControlLabel control={<Checkbox value="checkbox2" />} label="Checkbox 2" />
+                                    <FormControlLabel control={<Checkbox value="checkbox3" />} label="Checkbox 3"/>
+                                </FormGroup>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControl>
+                                    <FormLabel>Gender</FormLabel>
+                                    <RadioGroup
+                                        value={this.state.genderRadio}
+                                        onChange={this.genderRadioFill}
+                                        aria-label="Gender"
+                                        name="gender1">
+                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                        <FormControlLabel value="other" control={<Radio />} label="Other" />
+                                        <FormControlLabel
+                                            value="disabled"
+                                            disabled
+                                            control={<Radio />}
+                                            label="(Disabled option)"
+                                        />
+                                    </RadioGroup>
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Bio" fullWidth multiline rows={3}/>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                    <CardActions>
+                        <Grid container spacing={8} justify="flex-end">
+                            <Grid item xs={1}><Button color="primary" type="submit" fullWidth variant="raised" children="Save"/></Grid>
+                            <Grid item xs={1}><Button color="primary" onClick={event =>{this.goToUrl("/user", event)}} fullWidth variant="raised" children="Cancel"/></Grid>
+                        </Grid>
+                    </CardActions>
+                </Card>
+            </form>
+        );
+        return registrationForm;
     }
 }
 
