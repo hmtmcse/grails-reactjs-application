73cf0248f38697cabb5df8439a55fcdcaad259ff
Touhid Mia <hmtm.cse@gmail.com>
2018-12-21 10:34:38 +0600

    User Navigations

--- react-app/src/artifacts/ra-table-action.js
+++ react-app/src/artifacts/ra-table-action.js
@@ -2,137 +2,110 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
-import {ListItemIcon, MenuItem} from "@material-ui/core";
-import DraftsIcon from '@material-ui/icons/Drafts';
+import {ListItemIcon, MenuItem, Icon} from "@material-ui/core";
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Edit from '@material-ui/icons/Edit';
 import Delete from '@material-ui/icons/Delete';
 import Visibility from '@material-ui/icons/Visibility';
-import RaPagination from "./ra-pagination";
+import ErrorIcon from '@material-ui/icons/Error';
+import _ from 'lodash';
 
-const ITEM_HEIGHT = 48;
-
-const options = [
-    'None',
-    'Atria',
-    'Callisto',
-    'Dione'
-];
 
 export default class RaTableAction extends Component {
+
+    constructor(props) {
+        super(props);
+    }
 
     state = {
         anchorEl: null,
     };
 
     handleClick = event => {
-        this.setState({ anchorEl: event.currentTarget });
+        this.setState({anchorEl: event.currentTarget});
     };
 
     handleClose = () => {
-        this.setState({ anchorEl: null });
+        this.setState({anchorEl: null});
     };
 
     render() {
-        const { anchorEl } = this.state;
+        const {anchorEl} = this.state;
         const open = Boolean(anchorEl);
-        const { editAction, viewAction, deleteAction } = this.props;
-        let { others } = this.props;
-        if (editAction){
-            others.push(new ActionDefinition("Edit", editAction, Edit))
-        }
-
-
-
-        if (viewAction){
-            others.push(new ActionDefinition("View", viewAction, Visibility))
-        }
-
-        if (deleteAction){
-            others.push(new ActionDefinition("Delete", deleteAction, Delete))
-        }
-
-        console.log(others);
-
-        let view = <React.Fragment/>;
-        if (others.length !== 0){
-            view = (
-                <React.Fragment>
-                    <IconButton
-                        aria-label="More"
-                        aria-owns={open ? 'long-menu' : undefined}
-                        aria-haspopup="true"
-                        onClick={this.handleClick}>
-                        <MoreVertIcon />
-                    </IconButton>
-                    <Menu
-                        id="long-menu"
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={this.handleClose}
-                        PaperProps={{
-                            style: {
-                                maxHeight: ITEM_HEIGHT * 4.5,
-                                width: 200,
-                            },
-                        }}>
-                        {options.map(option => (
-                            <MenuItem key={option} onClick={this.handleClose}>
-                                <ListItemIcon >
-                                    <Edit/>
-                                </ListItemIcon>
-                                {option}
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                </React.Fragment>
-            );
-        }
-        return view;
+        const {tableActions} = this.props;
+        return (
+            <React.Fragment>
+                <IconButton
+                    aria-label="More"
+                    aria-owns={open ? 'long-menu' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleClick}>
+                    <MoreVertIcon/>
+                </IconButton>
+                <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={this.handleClose}
+                    PaperProps={{
+                        style: {
+                            minWidth: 150,
+                        },
+                    }}>
+                    {
+                        _.map(tableActions, (actionDefinition, key) => {
+                            return (
+                                <MenuItem key={key}>
+                                    <ListItemIcon>
+                                        {actionDefinition.icon ? (<actionDefinition.icon/>) : (<ErrorIcon/>)}
+                                    </ListItemIcon>
+                                    {actionDefinition.label}
+                                </MenuItem>
+                            )
+                        })
+                    }
+                </Menu>
+            </React.Fragment>
+        );
     }
 }
+RaTableAction.propTypes = {
+    tableActions: PropTypes.object.isRequired,
+};
 
-class ActionDefinition {
+
+export class ActionDefinition {
 
     label = "";
     action = undefined;
-    icon = "";
-    confirmation = {
-        okayLabel: "Okay",
-        okayFunction: null,
-        cancelFunction: null,
-        cancelLabel: "Cancel",
-    };
+    icon = ErrorIcon;
+    confirmation = undefined;
 
     constructor(label, action, icon) {
         this.label = label;
         this.action = action;
         this.icon = icon;
     }
+
+    addConfirmation() {
+        this.confirmation = {
+            okayLabel: "Okay",
+            okayFunction: null,
+            cancelFunction: null,
+            cancelLabel: "Cancel",
+        };
+    }
+
+    static commonActions() {
+        return {
+            viewAction: ActionDefinition.instance("View", undefined, Visibility),
+            editAction: ActionDefinition.instance("Edit", undefined, Edit),
+            deleteAction: ActionDefinition.instance("Delete", undefined, Delete),
+        }
+    }
+
+
+    static instance(label, action, icon) {
+        return new ActionDefinition(label, action, icon)
+    }
 }
-
-let actionMap = {
-  label: "",
-  action: "",
-  icon: DraftsIcon,
-  confirmation: {
-      okayLabel: "Okay",
-      okayFunction: null,
-      cancelFunction: null,
-      cancelLabel: "Cancel",
-  },
-};
-
-RaTableAction.defaultProps = {
-    editAction: undefined,
-    viewAction: undefined,
-    deleteAction: undefined,
-    others: [],
-};
-
-RaTableAction.propTypes = {
-    editAction: PropTypes.func,
-    viewAction: PropTypes.func,
-    deleteAction: PropTypes.func,
-    others: PropTypes.array,
-};
--- react-app/src/views/user/user-main-view.js
+++ react-app/src/views/user/user-main-view.js
@@ -9,7 +9,7 @@ import {withStyles} from '@material-ui/core/styles';
 import RaTableHeader from './../../artifacts/ra-table-header';
 import RaPagination from './../../artifacts/ra-pagination';
 import UserCreateUpdateView from './user-create-update-view';
-import RaTableAction from "../../artifacts/ra-table-action";
+import RaTableAction, {ActionDefinition} from "../../artifacts/ra-table-action";
 
 
 export const UserOtherUrls = [
@@ -118,9 +118,7 @@ class UserMainView extends RaViewComponent {
                                     <TableRow key={key}>
                                         <TableCell>{user.firstName} {user.lastName}</TableCell>
                                         <TableCell>{user.email}</TableCell>
-                                        <TableCell numeric><RaTableAction editAction={event =>{
-                                            console.log("Edit")
-                                        }}/></TableCell>
+                                        <TableCell numeric><RaTableAction tableActions={ActionDefinition.commonActions()}/></TableCell>
                                     </TableRow>
                                 )
                             })}
